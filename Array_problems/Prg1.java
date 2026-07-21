87503782a9d61890156b9a92ec9c0947d81d6f4b
Tauz Ul Imam <tauzulimam@gmail.com>
2026-07-21 22:14:01 +0530

    Array passing
    Array passing over method...

---
+++ Array_problems/Prg1.java
@@ -0,0 +1,24 @@
+//Array passing
+
+class Prg1
+{
+public static void main(String args[])
+{
+int arr[]={1,2,4,5,6,};
+printarray(arr);
+
+
+
+}
+
+static void printarray(int [] n)
+{
+for(int num:n)
+{
+System.out.print(num+" ");
+}
+
+
+}
+
+}
