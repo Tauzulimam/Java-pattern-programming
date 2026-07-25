fc65a539cd8d9da8cdea1d6ee21e396e8a8e0e95
Tauz Ul Imam <tauzulimam@gmail.com>
2026-07-25 23:43:22 +0530

    Create a program for string concatenation
    Add a Java program to concatenate first and last names.

---
+++ Array_problems/prg10.java
@@ -0,0 +1,12 @@
+//Concatinating a string 
+class Conatination{
+public static void main(String [] args)
+  {
+    String firstName = "Java";
+  String lastName = "Developer";
+  
+  String fullName = firstName + " " + lastName;
+  
+  System.out.println(fullName); // Output: Java Developer
+  }
+}
